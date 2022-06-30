fceee6e267ad972abd4d0c153cb73fec974fa028
Tuukka <hartikainentuukka@gmail.com>
2022-06-30 08:49:07 +0300

    kaikki ominaisuudet toimii tietokannan kanssa

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
+
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
@@ -25,17 +26,23 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response)=>{
+app.get('/api/notes/:id', (request, response, next)=>{
   Note.findById(request.params.id).then(note => {
-    response.json(note)
+    if(note) {
+      response.json(note)
+    }else{
+      response.status(404).end()
+    }
   })
+  .catch(error =>next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) =>{
-    const id = Number(request.params.id)
-    notes = Note.filter(note => note.id !== id)
-
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) =>{
+    Note.findByIdAndRemove(request.params.id)
+      .then(result =>{
+        response.status(204).end()
+      })
+      .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response)=>{
@@ -58,11 +65,37 @@ app.post('/api/notes', (request, response)=>{
     })
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError'){
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
