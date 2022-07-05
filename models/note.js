f78cee4c030720b9d771db9f7969162c801e8565
Tuukka <hartikainentuukka@gmail.com>
2022-07-05 13:26:58 +0300

    koodi refaktoroitu

--- models/note.js
+++ models/note.js
@@ -1,37 +1,28 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 
 const noteSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    minlength: 5,
-    required: true
+
+    content: {
+        type: String,
+        minlength: 5,
+        required: true
+        // eslint-disable-next-line indent
   },
-  date: { 
-    type: Date,
-    required: true
-  },
+    date: { 
+        type: Date,
+        required: true
+    },
     important: Boolean,
-  })
-  const Note = mongoose.model('Note', noteSchema)
+})
   
-  noteSchema.set('toJSON',{
+noteSchema.set('toJSON',{
     transform: (document, returnedObject) =>{
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
-  })
+})
   
-  module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model('Note', noteSchema)
